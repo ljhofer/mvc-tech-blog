1c951906fb7342cb5a5bf8f65c2653502a174e44
Laura Hofer <ljhofer@gmail.com>
2021-11-02 13:07:33 -0500

    Deleted extraneous comments

--- controllers/index.js
+++ controllers/index.js
@@ -11,32 +11,4 @@ module.exports = router;
 
 
 
-// Page Routes // 
-
-
-
-// Add entry
-
-
-
-// Data Routes //
-
-// Check log in credentials against known users -- needs login.js
-
-// Sign up - Post new user -- needs signin.js? 
-
-// GET all entries
-
-// Get single entry (by id?)
-
-// GET all my entries
-
-// GET one entry by ID
-
-// Add entry -- needs add-entry.js
-
-// Add comment -- needs add-comment.js 
-
-
-// Which of these need a JS file? Every click or just forms??
 
